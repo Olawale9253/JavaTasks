0fa76bb9b2683000161385c9196d4468e680e4d8
Taofeek Olawale <taofeekolawale159@gmail.com>
2026-04-06 12:07:12 +0100

    Update Task

---
+++ EasterEggs/HotDog/DecreasingOrder.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+    public class DecreasingOrder{
+
+    public static void main(String args[]){
+
+    Scanner UserInput = new Scanner(System.in);
+
+    System.out.print("Enter first integer: ");
+
+    int FirstInteger = UserInput.nextInt();
+
+    System.out.print("Enter second integer: ");
+
+    int SecondInteger = UserInput.nextInt();
+
+    System.out.print("Enter third integer: ");
+
+    int ThirdInteger = UserInput.nextInt();
+
+    System.out.println("Decreasing number")
+
+
+    
+}
+}
