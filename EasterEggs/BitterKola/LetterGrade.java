3d88fdc7c15ae33c73e27ed18f4be171c2d08913
Taofeek Olawale <taofeekolawale159@gmail.com>
2026-04-07 13:19:08 +0100

    Update Task

--- EasterEggs/BitterKola/LetterGrade.java
+++ EasterEggs/BitterKola/LetterGrade.java
@@ -6,38 +6,35 @@ import java.util.Scanner;
     
     Scanner UserInput = new Scanner(System.in);
 
-    System.out.print("Enter your grade: ");
+        System.out.print("Enter your grade: ");
 
     String Grade = UserInput.nextLine();
 
     if (Grade.equals("A")){
     
-    System.out.println("Your GPA is 4.0");
+        System.out.println("Your GPA is 4.0");
 }
 
     else if (Grade.equals("B") ){
 
-    System.out.println("Your GPA is 3.0");
+        System.out.println("Your GPA is 3.0");
 }
-
     else if (Grade.equals("C") ){
 
-    System.out.println("Your GPA is 2.0");
+        System.out.println("Your GPA is 2.0");
 }
-
     else if (Grade.equals("D") ){
 
-    System.out.println("Your GPA is 1.0");
+        System.out.println("Your GPA is 1.0");
 }
     else if (Grade.equals ("F") ){
     
-    System.out.println("Your GPA is 0.0");
-
+        System.out.println("Your GPA is 0.0");
+        
 }
-
     else{
     
-    System.out.println("Error input");
+        System.out.println("Error input");
 }
 }
 }
