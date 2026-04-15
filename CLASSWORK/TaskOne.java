0714afd7ee2dc06be93e02e7032a7467059f2bac
Olawale <taofeekolawale159@gmail.com>
2026-04-15 17:02:44 +0100

    New Task

---
+++ CLASSWORK/TaskOne.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+    public class TaskOne{
+
+    
+        public static void main(String args[]){
+
+        
+
+        Scanner userInput = new Scanner(System.in);      
+    
+        System.out.print("Enter Score: ");          
+
+        int Score = userInput.nextInt();
+
+        for(Score = 1; Score <= 10; Score++){
+    
+        int Score = 0;
+
+        while(Score <= 10){
+
+}
+
+        System.out.println("Sum" + Score++ );
+    
+      
+}
+
+
+        
+}
+
+}
