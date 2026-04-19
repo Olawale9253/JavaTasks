a4310fe4f75337c5ba55f43d2cc4f5405f7e43fb
Olawale <taofeekolawale159@gmail.com>
2026-04-19 17:24:59 +0100

    New Task Update

--- TOUCH GRASS/Multiplication.java
+++ TOUCH GRASS/Multiplication.java
@@ -5,11 +5,11 @@ int firstNumber = 5;
 int secondNumber = 1;
 
     for(firstNumber = 5; firstNumber == 5; firstNumber++){
-    System.out.println("Multiplication Table Five: " + firstNumber);
+    System.out.println("Multiplication Table 5");
 
       for(secondNumber = 1; secondNumber <= 12; secondNumber++){
 
-        System.out.println(firstNumber * secondNumber + " ");
+        System.out.println(firstNumber * secondNumber);
          }
     System.out.println();
 }
