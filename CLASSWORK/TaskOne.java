667e0bd01041293d84289f6a6e08c06063953740
Olawale <taofeekolawale159@gmail.com>
2026-04-19 17:17:28 +0100

    New Task Update

--- CLASSWORK/TaskOne.java
+++ CLASSWORK/TaskOne.java
@@ -5,23 +5,18 @@ import java.util.Scanner;
     
         public static void main(String args[]){
 
-        
-
         Scanner userInput = new Scanner(System.in);      
-    
-        System.out.print("Enter Score: ");          
-
-        int Score = userInput.nextInt();
-
-        for(Score = 1; Score <= 10; Score++){
-    
+        
         int Score = 0;
 
+        int Sum = 0;        
+
         while(Score <= 10){
+        
+        
 
-}
+        System.out.println(Score++);
 
-        System.out.println("Sum" + Score++ );
     
       
 }
---
+++ TOUCH GRASS/Multiplication.java
@@ -0,0 +1,17 @@
+public class Multiplication{
+
+    public static void main(String args[]){
+int number = 5;
+int j = 1;
+
+    for(number = 5; number == 5; number++){
+    System.out.println("Multiplication Table Five: " + number);
+
+      for(j = 1; j <= 12; j++){
+
+        System.out.println(number * j + " ");
+         }
+    System.out.println();
+}
+}
+}
