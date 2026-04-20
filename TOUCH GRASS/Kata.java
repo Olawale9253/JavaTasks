751cdb521da440d74b8ae0e43e26030760ba4211
Olawale <taofeekolawale159@gmail.com>
2026-04-20 08:25:24 +0100

    Task Almost Complete

---
+++ TOUCH GRASS/Kata.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Kata{
+
+    public static void main(String args[]){
+
+        Scanner userInput = new Scanner();
+
+        System.out.println("Enter an integer");
+
+        int number = userInput.nextInt();
+
+    if(userChoice == number){
+    
+        System.out.println()
+}
+
+
+}
+}
