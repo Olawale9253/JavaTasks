f227e052731a9dbf36dd9f91a86894a8a6a0aef9
Olawale <taofeekolawale159@gmail.com>
2026-04-28 08:30:06 +0100

    Task Updates

---
+++ ASSIGNMENT/ArraySnacks.java
@@ -0,0 +1,23 @@
+public class ArraySnacks {
+
+    public static int findLargest(int[] arr) {
+       
+        int largest = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+
+        
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {4, 9, 2, 15, 7};
+
+        int result = findLargest(numbers);
+
+        System.out.println("Largest element: " + result);
+    }
+}
