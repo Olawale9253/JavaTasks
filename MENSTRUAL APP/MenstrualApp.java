525d0e923ba9620c731b7cdb0c4bb6c7e8108fde
Olawale <taofeekolawale159@gmail.com>
2026-05-02 12:29:25 +0100

    Update Tasks

--- MENSTRUAL APP/MenstrualApp.java
+++ MENSTRUAL APP/MenstrualApp.java
@@ -2,13 +2,31 @@ public class MenstrualApp{
 
     public static void main(String args[]){
     
-        boolean menstrualapp  = true;
+//        boolean menstrualapp  = false;
 
 
-        while(menstrual){
+//        while(menstrualapp){
         
-        System.out.println("Welcome to your comfort zone ladies");
-}
+        System.out.println("===== MENSTRUAL CALCULATOR MINI APP ===== \n");
+
+        System.out.println("May 2026 \n");
+        
+        System.out.println(" S   |    M   |    T   |    W   |    T   |    F   |    S \n ");
+
+        System.out.println(" 1   |    2   |    3   |    4   |    5   |    6   |    7 ");
+
+        System.out.println(" 8   |    9   |    10  |    11  |    12  |    13  |    14 ");
+
+        System.out.println(" 15  |    16  |    17  |    18  |    19  |    20  |    21 ");
+            
+        System.out.println(" 22  |    23  |    24  |    25  |    26  |    27  |    28 ");
+
+        System.out.println(" 29  |    30  |    31  |");
+
+
+
+            
+//}
 
 }
 
