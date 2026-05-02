447c702e82db7b8fdb2909a33ddaf56791210669
Olawale <taofeekolawale159@gmail.com>
2026-05-02 12:08:36 +0100

    Update Tasks

---
+++ MENSTRUAL APP/MenstrualApp.java
@@ -0,0 +1,15 @@
+public class MenstrualApp{
+
+    public static void main(String args[]){
+    
+        boolean menstrualapp  = true;
+
+
+        while(menstrual){
+        
+        System.out.println("Welcome to your comfort zone ladies");
+}
+
+}
+
+}
