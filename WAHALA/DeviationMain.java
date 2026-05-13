48dd954cb29052528d1cadf6b522fbe54daf899a
Olawale <taofeekolawale159@gmail.com>
2026-05-14 00:15:26 +0100

    Task Updates

--- WAHALA/DeviationMain.java
+++ WAHALA/DeviationMain.java
@@ -2,6 +2,8 @@ public class DeviationMain{
 
     public static void main(String args[]){
 
+    System.out.println("\t Formula for the work \n\n variance = sum of mean deviation / total frequency \n mean = sum of number / number of number \n sum = add all numbers ");
+
 }
 
 }
